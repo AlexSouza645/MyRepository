1c0cf8e8a8aa7769b3a7ce572988cd315a52c606
alex miranda de souza <59890212+AlexSouza645@users.noreply.github.com>
2024-03-07 10:47:17 -0300

    atualizando dados

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 function scrollToElement(elementSelector, instance = 0) {
   // Select all elements that match the given selector
   const elements = document.querySelectorAll(elementSelector);
-  // check if there are elements matching the selectorand if the requested instance exists
+  // Check if there are elements matching the selector and if the requested instance exists
   if (elements.length > instance) {
-    // Scroll to the specified instance of the element
-    elements[instance].scrollIntoView({ behavior: "smooth" });
+      // Scroll to the specified instance of the element
+      elements[instance].scrollIntoView({ behavior: 'smooth' });
   }
 }
 
-const link1 = document.querySelector("#link1");
-const link2 = document.querySelector("#link2");
-const link3 = document.querySelector("#link3");
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
 
-link1.addEventListener("click", () => {
-  scrollToElement(".header");
+link1.addEventListener('click', () => {
+  scrollToElement('.header');
 });
 
-// scroll to the second element with header class
-link2.addEventListener("click", () => {
-  scrollToElement(".header", 1);
+link2.addEventListener('click', () => {
+  // Scroll to the second element with "header" class
+  scrollToElement('.header', 1);
 });
 
-link3.addEventListener("click", () => {
-  scrollToElement(".column");
+link3.addEventListener('click', () => {
+  scrollToElement('.column');
 });
